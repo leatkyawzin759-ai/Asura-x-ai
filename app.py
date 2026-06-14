abd65f73374b4b768e00a6051cdd3b29b6e079cb
leatkyawzin759-ai <leatkyawzin759@gmail.com>
2026-06-14 16:05:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
+
 st.title('Asura x AI App')
 st.write('အောင်မြင်စွာ တက်လာပါပြီ!')
+
+# အောက်ပါ Code က ညီကို့ကို စာရိုက်လို့ရစေမှာပါ
+name = st.text_input('မင်းနာမည်လေး ပြောပြပေးပါ')
+if name:
+    st.write(f'မင်္ဂလာပါ {name}! Asura AI က ကြိုဆိုပါတယ်။')
+  
